c32a6fd47d504f82e24d4f544842bc164761eb8d
Aniket Biradar <aniketbiradar1890@gmail.com>
2024-02-21 03:15:22 +0530

    Login Page created
    This Login Page features strong password validation and dynamic redirection based on user roles. Users are directed to either the Buyer Home or Farmer Home page upon successful login, enhancing security and providing a personalized experience.

--- bhumiputra/src/main/java/bhumiputra/LoginController.java
+++ bhumiputra/src/main/java/bhumiputra/LoginController.java
@@ -1,5 +1,48 @@
 package bhumiputra;
 
-public class LoginController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
+		UserCRUD crud = new UserCRUD();
+
+		try {
+			String dbpassword = crud.login(email);
+			String role = crud.getUserRole(email);
+
+			if (dbpassword != null) {
+				if (password.equals(dbpassword)) {
+
+					if ("buyer".equals(role)) {
+						resp.sendRedirect("BuyerHome.jsp");
+					} else if ("seller".equals(role)) {
+						resp.sendRedirect("FarmerHome.jsp");
+					} else {
+						// Handle other roles or redirect to a default page
+						resp.sendRedirect("defaultHome.jsp");
+					}
+
+				} else {
+					req.setAttribute("message", "Login Failed Invalid Password");
+					req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+				}
+			} else {
+				req.setAttribute("message", "User not resiter ,Plesse Register");
+				req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
--- bhumiputra/src/main/java/bhumiputra/UserCRUD.java
+++ bhumiputra/src/main/java/bhumiputra/UserCRUD.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class UserCRUD {
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
@@ -17,8 +16,8 @@ public class UserCRUD {
 
 	public int signup(User user) throws ClassNotFoundException, SQLException {
 		Connection connection = getConnection();
-		PreparedStatement statement = connection
-				.prepareStatement("INSERT INTO USER(id,first_name,last_name,email,phone,address,password,role) VALUES(?,?,?,?,?,?,?,?)");
+		PreparedStatement statement = connection.prepareStatement(
+				"INSERT INTO USER(id,first_name,last_name,email,phone,address,password,role) VALUES(?,?,?,?,?,?,?,?)");
 
 		statement.setInt(1, user.getId());
 		statement.setString(2, user.getFirst_name());
@@ -34,18 +33,47 @@ public class UserCRUD {
 		return count;
 	}
 
-	 public static int retrieveMaxIdFromDatabase() throws ClassNotFoundException, SQLException {
-	        try (Connection connection = getConnection();
-	             PreparedStatement statement = connection.prepareStatement("SELECT MAX(id) FROM USER");
-	             ResultSet resultSet = statement.executeQuery()) {
+	public static int retrieveMaxIdFromDatabase() throws ClassNotFoundException, SQLException {
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT MAX(id) FROM USER");
+				ResultSet resultSet = statement.executeQuery()) {
 
-	            if (resultSet.next()) {
-	                return resultSet.getInt(1);
-	            }
+			if (resultSet.next()) {
+				return resultSet.getInt(1);
+			}
 
-	            return 0; // Default to 0 if no records or an error occurs
-	        }
-	    }
+			return 0;
+		}
+	}
 
+	public String login(String email) throws ClassNotFoundException, SQLException {
+		Connection connection = getConnection();
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM USER WHERE email=?");
+
+		statement.setString(1, email);
+		ResultSet set = statement.executeQuery();
+		String password = null;
+		if (set.next()) {
+			password = set.getString("password");
+		}
+		connection.close();
+		return password;
+	}
+
+	public String getUserRole(String email) throws ClassNotFoundException, SQLException {
+		Connection connection = getConnection();
+		PreparedStatement statement = connection.prepareStatement("SELECT role FROM USER WHERE email=?");
+
+		statement.setString(1, email);
+		ResultSet resultSet = statement.executeQuery();
+		String role = null;
+
+		if (resultSet.next()) {
+			role = resultSet.getString("role");
+		}
+
+		connection.close();
+		return role;
+	}
 
 }
